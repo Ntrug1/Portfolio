5448d340c109634bcadb9263a6fa7df89d4900cf
Ntrug1 <quenpassroi1@gmail.com>
2022-05-01 18:24:55 +0700

    code base init

---
+++ components/masthead.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+const Masthead: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="p-12 font-bold">
+        <h1 className="mb-6 text-4xl xl:text-5xl">
+          Trung
+        </h1>
+          <h2>
+            <span>Hello</span>
+          </h2>
+      </div>
+    </div>
+  )
+}
+
+export default Masthead
