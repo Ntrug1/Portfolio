8966843c22e97730a9308a755f5ff96ac5c0961c
Ntrug1 <quenpassroi1@gmail.com>
2022-11-07 17:34:53 +0700

    change minor display

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const AboutMe: React.FC = () => {
   return (
-    <section className='flex flex-col bg-white py-20 text-3xl md:text-4xl'>
-      <div className='container mx-auto px-11 '>
-        <p className='leading-tight max-w-5xl mx-auto text-4xl tracking-tight'>
-          <strong>
-            About me 
-          </strong> 
+    <section className="flex flex-col py-20 text-3xl md:text-4xl">
+      <div className="container mx-auto px-11 ">
+        <p className="leading-tight max-w-5xl mx-auto text-4xl tracking-tight">
+          <strong>About me</strong>
         </p>
       </div>
-      <div className='container mx-auto px-11 text-center mt-28'>
-        <h2></h2>
-        
+      <div className="container mx-auto px-11 text-center mt-28">
+        <h2>Hello</h2>
       </div>
     </section>
-  )
+  );
+};
 
-}
-
-export default AboutMe
+export default AboutMe;
--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,27 +1,37 @@
-import React, {useRef, useContext, useState, useCallback} from "react";
+import React, { useRef, useContext, useState, useCallback } from "react";
 import { ArrowDown2 } from "iconsax-react";
 
 const Masthead: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <video autoPlay loop muted playsInline className="absolute w-full h-full object-cover ">
-        <source src="/assets/background-gif.mp4" type="video/mp4; codecs=hvc1" />
-        <source src="/assets/background-gif.webm" type="video/webm; codecs=vp9" />
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover "
+      >
+        <source
+          src="/assets/background-gif.mp4"
+          type="video/mp4; codecs=hvc1"
+        />
+        <source
+          src="/assets/background-gif.webm"
+          type="video/webm; codecs=vp9"
+        />
       </video>
-      <div className="p-12 font-bold z-10 text-white text-center drop-shadow[0_5px_3px_rgba(0,0,0,0.4)] flex-1 flex items-center justify-center flex-col">
-        <h1 className="mb-6 text-4xl xl:text-5xl">
-          Trung
-        </h1>
-          <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
-            <span>Hello,</span>
-            <span> I'm fresher front-end developer</span>
-          </h2>
+      <div className="p-12 font-bold z-10 text-center drop-shadow[0_5px_3px_rgba(0,0,0,0.4)] flex-1 flex items-center justify-center flex-col">
+        <h1 className="mb-6 text-4xl xl:text-5xl">Trung</h1>
+        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
+          <span>Hello,</span>
+          <span>I&apos;m fresher front-end developer</span>
+        </h2>
       </div>
       <div className="flex-grow-0 pb-20 md:pb-10 transition-all duration-1000 z-10">
-        <ArrowDown2 size={64} color='white' />
+        <ArrowDown2 size={64} color="white" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Masthead
+export default Masthead;
