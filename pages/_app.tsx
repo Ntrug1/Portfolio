4b509d83546020851eac27bcde51f8ae3a88061d
Ntrug1 <quenpassroi1@gmail.com>
2023-01-03 10:20:38 +0700

    minor update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import ScrollObserver from "../utils/contexts/ScrollObserver";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ScrollObserver>
-      <Component {...pageProps} />
-    </ScrollObserver>
+    // <ScrollObserver>
+    <Component {...pageProps} />
+    // </ScrollObserver>
   );
 }
 
